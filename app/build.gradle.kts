8014669ac9d4f744eb40e67fcede6281c8575ea0
Galvesh <galvezssr@gmail.com>
2025-06-05 14:05:50 +0200

    Added dependencies, firebase analytics and crashlytics

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,12 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlin.serializacion)
+    alias(libs.plugins.gservices)
+    alias(libs.plugins.firebase.crashlytics)
 }
 
 android {
@@ -56,4 +62,28 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Retrofit and Json-converter
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
+    // Dagger hilt
+    implementation(libs.dagger.hilt)
+    ksp(libs.dagger.hilt.compiler)
+    implementation(libs.dagger.hilt.navigation)
+
+    // Coil
+    implementation(libs.coil)
+
+    // Paging
+    implementation(libs.paging)
+
+    // Navigation-compose
+    implementation(libs.navigation.compose)
+    implementation(libs.kotlinx.serliazation)
+
+    // Firebase
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
+    implementation(libs.firebase.crashlytics)
 }
--- app/src/main/java/com/galvezsh/rickandmortydb/MainActivity.kt
+++ app/src/main/java/com/galvezsh/rickandmortydb/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,6 +13,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.galvezsh.rickandmortydb.ui.theme.RickAndMortyDBTheme
+import com.google.firebase.Firebase
+import com.google.firebase.FirebaseApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,7 +37,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier.clickable { throw RuntimeException("Test de escepcion") }
     )
 }
 
